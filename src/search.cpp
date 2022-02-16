76cc0caf0858eb122a8b270f4f028108972940aa
alabx <97296327+alabx@users.noreply.github.com>
2022-02-16 20:35:08 +0100

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1068,7 +1068,7 @@ moves_loop: // When in check, search starts here
               &&  tte->depth() >= depth - 3)
           {
               Value singularBeta = ttValue - 4 * depth;
-              Depth singularDepth = (depth - 1) / 2;
+              Depth singularDepth = (depth - 1 + PvNode) / 2;
 
               ss->excludedMove = move;
               value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
