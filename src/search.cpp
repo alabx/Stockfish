9e5856e3b6250d569489bb02109349af27141304
alabx <97296327+alabx@users.noreply.github.com>
2022-07-29 17:36:43 +0200

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1063,7 +1063,7 @@ moves_loop: // When in check, search starts here
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
               &&  abs(ttValue) < VALUE_KNOWN_WIN
               && (tte->bound() & BOUND_LOWER)
-              &&  tte->depth() >= depth - 3)
+              &&  tte->depth() >= depth - 4)
           {
               Value singularBeta = ttValue - 3 * depth;
               Depth singularDepth = (depth - 1) / 2;
