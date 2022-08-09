4170a116b0388a58edad9b99671ee8f0620dee95
alabx <97296327+alabx@users.noreply.github.com>
2022-08-09 19:21:31 +0200

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 2;
+              r++;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
